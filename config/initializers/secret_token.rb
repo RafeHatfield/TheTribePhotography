15757e7c41226050c2758d54461495e49627653f
Rafe Hatfield <rafe.hatfield@gmail.com>
2011-11-05 15:11:12 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thetribephotography::Application.config.secret_token = 'b82e88631806218849ad71b2af6095948476c8af93f2fc7eab640aea3b1532fc5d1c1e1ebcc78a9fe58ab89b41f0ecb484aa90bc58cffe4cc8a12a7842402485'
