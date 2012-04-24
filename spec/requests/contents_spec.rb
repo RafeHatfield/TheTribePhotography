e5f4b53451d6a17edc5366f4a95de4c013cdae4c
Rafe Hatfield <rafe.hatfield@gmail.com>
2012-04-24 06:30:30 -0700

    tidied up specs

--- spec/requests/contents_spec.rb
+++ spec/requests/contents_spec.rb
@@ -5,61 +5,60 @@ describe "contents" do
   let(:base_title) { "The Tribe Photography" }
 
   describe "Home page" do
+    
+    before { visit home_path }
 
     it "should have the h1 'The Tribe Photography'" do
-      visit home_path
       page.should have_selector('span', :text => 'The Tribe Photography')
     end
 
     it "should have the base title" do
-      visit home_path
       page.should have_selector('title',
                         :text => "#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit home_path
       page.should_not have_selector('title', :text => '| Home')
     end
   end
 
   describe "contact page" do
+    
+    before { visit contact_path }
 
     it "should have the h1 'Contact'" do
-      visit contact_path
       page.should have_selector('h1', :text => 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
       page.should have_selector('title',
                         :text => "#{base_title} | Contact")
     end
   end
 
   describe "About page" do
+    
+    before { visit about_path }
 
     it "should have the h1 'About'" do
-      visit about_path
       page.should have_selector('h1', :text => 'About')
     end
 
     it "should have the title 'About Us'" do
-      visit about_path
       page.should have_selector('title',
                     :text => "#{base_title} | About")
     end
   end
   
   describe "Gallery page" do
+    
+    before { visit gallery_path }
 
     it "should have the h1 'Gallery'" do
-      visit gallery_path
       page.should have_selector('h1', :text => 'Gallery')
     end
 
     it "should have the title 'Gallery'" do
-      visit gallery_path
       page.should have_selector('title',
                     :text => "#{base_title} | Gallery")
     end
