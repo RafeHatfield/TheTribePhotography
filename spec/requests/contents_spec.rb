c9b7f3f3dc2faa7e88488297e6fbd25d9f81b798
Rafe Hatfield <rafe.hatfield@gmail.com>
2012-04-25 23:05:25 -0700

    added shared examples to rspec tests

--- spec/requests/contents_spec.rb
+++ spec/requests/contents_spec.rb
@@ -5,6 +5,11 @@ describe "contents" do
   let(:base_title) { "The Tribe Photography" }
   subject { page }
 
+  shared_examples_for "all standard pages" do
+    it { should have_selector('h1',    text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     
     before { visit home_path }
@@ -17,24 +22,30 @@ describe "contents" do
   describe "contact page" do
     
     before { visit contact_path }
+    
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { should have_selector('h1', :text => 'Contact') }
-    it { should have_selector('title', :text => full_title('Contact')) }
+    it_should_behave_like "all standard pages"
   end
 
   describe "About page" do
     
     before { visit about_path }
+    
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
 
-    it { should have_selector('h1', :text => 'About') }
-    it { should have_selector('title', :text => full_title('About')) }
+    it_should_behave_like "all standard pages"
   end
   
   describe "Gallery page" do
     
     before { visit gallery_path }
+    
+    let(:heading) { 'Gallery' }
+    let(:page_title) { 'Gallery' }
 
-    it { should have_selector('h1', :text => 'Gallery') }
-    it { should have_selector('title', :text => full_title('Gallery')) }
+    it_should_behave_like "all standard pages"
   end
 end
