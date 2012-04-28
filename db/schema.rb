d38fa018ddb0a0d31029e99637a7ca9ef0f4773d
Rafe Hatfield <rafe.hatfield@gmail.com>
2012-04-28 10:02:12 -0700

    added user model validations, index and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120427043037) do
+ActiveRecord::Schema.define(:version => 20120428165550) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(:version => 20120427043037) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
