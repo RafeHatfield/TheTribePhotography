d7cdd7ef54f88f61431a847f1b0fa4e8c74763e4
Rafe Hatfield <rafe.hatfield@gmail.com>
2012-04-30 23:48:24 -0700

    finished user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Thanks for signing up!"
+      redirect_to @user
     else
       render 'new'
     end
