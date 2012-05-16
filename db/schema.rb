a9771041aad92251ce40ef82e10119b2f21562b0
Rafe Hatfield <rafe.hatfield@suite101.com>
2012-05-16 08:38:48 -0700

    user list and pagination

--- db/schema.rb
+++ db/schema.rb
@@ -13,16 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20120505222723) do
 
-  create_table "articles", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "published_at"
-    t.string   "excerpt"
-    t.binary   "active"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-  end
-
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
