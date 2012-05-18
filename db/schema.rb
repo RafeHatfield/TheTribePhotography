b787ecfa8e3707bd77e2f0d8b2f6e9e14d53b9fd
Rafe Hatfield <rafe.hatfield@gmail.com>
2012-05-17 22:54:34 -0700

    removing user admin header until further development

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,16 @@
 
 ActiveRecord::Schema.define(:version => 20120516160403) do
 
+  create_table "articles", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "published_at"
+    t.string   "excerpt"
+    t.binary   "active"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
